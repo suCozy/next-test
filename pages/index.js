6355143e157fed3135576a0780e46897e2d6b5dd
suCozy <cozynye@vetplace.co.kr>
2022-10-28 17:07:40 +0900

    develop branch

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>develop!!!!!!! branch</div>
+    <div>!!!!!!develop!!!!!!! branch</div>
   )
 }
